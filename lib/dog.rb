6d0e2971bc41f32ad73d71ebe9107cef13cbb815
Allene Norton <norton.allene@gmail.com>
2020-07-14 19:24:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name
+    @name
+  end 
+  def name=(nu_name)
+    @name = nu_name
+  end 
+  def breed 
+    @breed 
+  end 
+  def breed=(nu_breed)
+    @breed = nu_breed
+  end 
+end 
